60adbaa8ab517e4375a37bc76d7d73a5a3f76aba
Omar Garcia <og4115244@gmail.com>
2024-02-18 21:05:26 -0500

    day 4

--- Day4/BankerRoulette.py
+++ Day4/BankerRoulette.py
@@ -1,2 +1,8 @@
+import random
+names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
 
-print("numbers[1]")
+people = (len(names)-1)
+
+randomInt = random.randint(0, people)
+
+print(names[randomInt] + " is going to buy the meal today!")
